d6d866af21f0bdb18792213d87aebc08cf7cf07c
LujainAlattar <LujainA.Alattar@gmail.com>
2023-07-08 13:10:38 +0300

    Your commit message

---
+++ wordpress/wp-content/uploads/sucuri/sucuri-auditqueue.php
@@ -0,0 +1,7 @@
+<?php
+// datastore=auditqueue;
+// created_on=1688727179;
+// updated_on=1688727179;
+exit(0);
+?>
+1688727179_9326:"Debug: admin, 127.0.0.1; Sucuri plugin has been uninstalled"
---
+++ wordpress/wp-content/uploads/sucuri/sucuri-ignorescanning.php
@@ -0,0 +1,6 @@
+<?php
+// datastore=ignorescanning;
+// created_on=1688727179;
+// updated_on=1688727179;
+exit(0);
+?>
